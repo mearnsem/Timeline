b2a394d9be05f02e8620bae61f1b5d5045b7c05b
Emily Mearns <mearnsem@gmail.com>
2016-06-21 15:29:25 -0600

    Model Object Controller

---
+++ Timeline/PostController.swift
@@ -0,0 +1,37 @@
+//
+//  PostController.swift
+//  Timeline
+//
+//  Created by Emily Mearns on 6/21/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    static let sharedController = PostController()
+    
+    func createPost(photo: UIImage, caption: String) {
+        guard let data = UIImageJPEGRepresentation(photo, 0.8) else {return}
+        let post = Post(photo: data)
+        
+        addCommentToPost(caption, post: post)
+        saveContext()
+    }
+    
+    func addCommentToPost(text: String, post: Post) {
+        let _ = Comment(post: post, text: text)
+        saveContext()
+    }
+    
+    func saveContext() {
+        let moc = Stack.sharedStack.managedObjectContext
+        do {
+            try moc.save()
+        } catch {
+            print("Context could not be saved")
+        }
+    }
+    
+}
