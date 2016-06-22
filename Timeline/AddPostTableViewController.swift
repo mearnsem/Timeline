734e8683deddf31059364d8a1eea5a74791a3c3e
Emily Mearns <mearnsem@gmail.com>
2016-06-22 16:54:43 -0600

    Photo Select Child Scene

--- Timeline/AddPostTableViewController.swift
+++ Timeline/AddPostTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddPostTableViewController: UITableViewController {
+class AddPostTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var captionTextField: UITextField!
@@ -20,9 +20,39 @@ class AddPostTableViewController: UITableViewController {
     // MARK: - IBActions
     
     @IBAction func selectImageButtonPressed(sender: AnyObject) {
-        postImageView.image = UIImage(named: "pic")
-        self.title = ""
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Please choose image source", message: nil, preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        actionSheet.addAction(cancelAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .Default) { (_) in
+            imagePicker.sourceType = .PhotoLibrary
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            actionSheet.addAction(photoLibraryAction)
+        }
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .Default) { (_) in
+            imagePicker.sourceType = .Camera
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            actionSheet.addAction(cameraAction)
+        }
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {return}
+        postImageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     @IBAction func addPostButtonPressed(sender: AnyObject) {
         if let image = postImageView.image, text = captionTextField.text {
             PostController.sharedController.createPost(image, caption: text)
