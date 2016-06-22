bf3ac097417be694d1cde613dacaad2b3118987e
Emily Mearns <mearnsem@gmail.com>
2016-06-22 17:18:15 -0600

    Protocol Extension

---
+++ Timeline/CloudKitManagedObject.swift
@@ -0,0 +1,58 @@
+//
+//  CloudKitManagedObject.swift
+//  Timeline
+//
+//  Created by Emily Mearns on 6/22/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import CoreData
+
+@objc protocol CloudKitManagedObject {
+    
+    var timestamp: NSDate {get set}
+    var recordIDData: NSData? {get set}
+    var recordName: String {get set}
+    var recordType: String {get}
+    var cloudKitRecord: CKRecord? {get}
+    
+    func updateWithRecord(record: CKRecord)
+    
+    init?(record: CKRecord, context: NSManagedObjectContext)
+    
+}
+
+extension CloudKitManagedObject {
+    
+    var isSynced: Bool {
+        return recordIDData != nil
+    }
+    
+    var cloudKitRecordID: CKRecordID? {
+        guard let recordIDData = recordIDData, let recordID = NSKeyedUnarchiver.unarchiveObjectWithData(recordIDData) as? CKRecordID else {return nil}
+        return recordID
+    }
+    
+    var cloudKitReference: CKReference? {
+        guard let cloudKitRecordID = cloudKitRecordID else {return nil}
+        return CKReference(recordID: cloudKitRecordID, action: .None)
+    }
+    
+    func update(record: CKRecord) {
+        self.recordIDData = NSKeyedArchiver.archivedDataWithRootObject(record.recordID)
+        
+        let moc = Stack.sharedStack.managedObjectContext
+        do {
+            try moc.save()
+        } catch {
+            print("Updated record could not be saved")
+        }
+    }
+    
+    func nameForManagedObject() -> String {
+        return NSUUID().UUIDString
+    }
+    
+}
